f4ff2461eadfaa3326432e4d44dbc590e8054f06
apintar <adampintar@gmail.com>
2016-02-28 21:45:22 -0500

    Initial commit
    First draft of package
    
    Basic functionality

---
+++ R/reflow_lines.R
@@ -0,0 +1,42 @@
+#' @title reflowLines
+#'
+#' @description reflowLines
+#'
+#' @export
+#'
+reflowLines <- function () {
+
+  tmp <- rstudioapi::getActiveDocumentContext()
+  start_pos <- tmp$selection[[1]]$range$start
+  end_pos <- tmp$selection[[1]]$range$end
+  if (start_pos[1] == end_pos[1] && start_pos[2] == end_pos[2]){
+
+    replacement_text <- stringr::str_wrap(tmp$contents[start_pos[1]], width = 70)
+    current_length <- nchar(tmp$contents[start_pos[1]])
+    replacement_range <- rstudioapi::document_range(start = c(start_pos[1], 1),
+                                                    end = c(start_pos[1], current_length))
+  } else {
+
+    if (nchar(tmp$contents[end_pos[1]]) == 0) {
+
+      replacement_text <- paste0(tmp$contents[start_pos[1]:(end_pos[1] - 1)],
+                                 collapse = ' ')
+      replacement_text <- stringr::str_wrap(replacement_text, width = 70)
+      last_length <- nchar(tmp$contents[end_pos[1] - 1])
+      replacement_range <- rstudioapi::document_range(start = c(start_pos[1], 1),
+                                                      end = c(end_pos[1] - 1, last_length))
+    } else {
+
+      replacement_text <- paste0(tmp$contents[start_pos[1]:end_pos[1]],
+                                 collapse = ' ')
+      cat(replacement_text)
+      replacement_text <- stringr::str_wrap(replacement_text, width = 70)
+      last_length <- nchar(tmp$contents[end_pos[1] - 1])
+      replacement_range <- rstudioapi::document_range(start = c(start_pos[1], 1),
+                                                      end = c(end_pos[1], last_length))
+    }
+  }
+
+  rstudioapi::modifyRange(location = replacement_range,
+                          text = replacement_text)
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## Installation
+
+Requires Rstudio version >= 0.99.878
+
+```r
+install.packages('devtools')
+devtools::install_github('rstudio/rstudioapi')
+devtools::install_github('Rexamine/stringi')
+devtools::install_github('hadley/stringr')
+```
