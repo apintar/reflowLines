d10f523f8a64664ade6a9e0b6d824b657a95741f
apintar <adampintar@gmail.com>
2016-04-27 17:13:44 -0400

    Indentation of the first line is now preserved and propagated to the rest of the lines.  This is important for example with nested lists in Rmd files.

--- R/reflow_lines.R
+++ R/reflow_lines.R
@@ -9,19 +9,26 @@ reflowLines <- function () {
   tmp <- rstudioapi::getActiveDocumentContext()
   start_pos <- tmp$selection[[1]]$range$start
   end_pos <- tmp$selection[[1]]$range$end
+
+  # count leading white space
+  lws <- nchar(tmp$contents[start_pos[1]]) -
+    nchar(stringr::str_trim(tmp$contents[start_pos[1]]))
   if (start_pos[1] == end_pos[1] && start_pos[2] == end_pos[2]){
 
-    replacement_text <- stringr::str_wrap(tmp$contents[start_pos[1]], width = 70)
+    replacement_text <- stringr::str_wrap(tmp$contents[start_pos[1]], width = 70,
+                                          indent = lws, exdent = lws)
     current_length <- nchar(tmp$contents[start_pos[1]])
     replacement_range <- rstudioapi::document_range(start = c(start_pos[1], 1),
                                                     end = c(start_pos[1], current_length + 1))
   } else {
 
     if (nchar(tmp$contents[end_pos[1]]) == 0) {
+      # a blank line is part of the selection (no spaces even)
 
       replacement_text <- paste0(tmp$contents[start_pos[1]:(end_pos[1] - 1)],
                                  collapse = ' ')
-      replacement_text <- stringr::str_wrap(replacement_text, width = 70)
+      replacement_text <- stringr::str_wrap(replacement_text, width = 70,
+                                            indent = lws, exdent = lws)
       last_length <- nchar(tmp$contents[end_pos[1] - 1])
       replacement_range <- rstudioapi::document_range(start = c(start_pos[1], 1),
                                                       end = c(end_pos[1] - 1, last_length + 1))
@@ -29,7 +36,8 @@ reflowLines <- function () {
 
       replacement_text <- paste0(tmp$contents[start_pos[1]:end_pos[1]],
                                  collapse = ' ')
-      replacement_text <- stringr::str_wrap(replacement_text, width = 70)
+      replacement_text <- stringr::str_wrap(replacement_text, width = 70,
+                                            indent = lws, exdent = lws)
       last_length <- nchar(tmp$contents[end_pos[1]])
       replacement_range <- rstudioapi::document_range(start = c(start_pos[1], 1),
                                                       end = c(end_pos[1], last_length + 1))
