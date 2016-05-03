6fd8292452e85036a8db1637659ccecc1c283b02
apintar <adampintar@gmail.com>
2016-05-03 10:58:27 -0400

    On branch master  Changes to be committed:
    	modified:   R/reflow_lines.R
    
      Changed the whitespace_only argument of stri_wrap to TRUE so that
      dollar signs that end a line are not wrapped to the next line

--- R/reflow_lines.R
+++ R/reflow_lines.R
@@ -1,3 +1,14 @@
+my_wrap <- function (string, width = 80, indent = 0, exdent = 0) {
+
+  if (width <= 0) {
+    width <- 1
+  }
+
+  out <- stringi::stri_wrap(string, width = width,
+                            indent = indent, exdent = exdent,
+                            whitespace_only = TRUE, simplify = FALSE)
+  vapply(out, stringr::str_c, collapse = "\n", character(1))
+}
 #' @title reflowLines
 #'
 #' @description reflowLines
@@ -15,8 +26,8 @@ reflowLines <- function () {
     nchar(stringr::str_trim(tmp$contents[start_pos[1]]))
   if (start_pos[1] == end_pos[1] && start_pos[2] == end_pos[2]){
 
-    replacement_text <- stringr::str_wrap(tmp$contents[start_pos[1]], width = 70,
-                                          indent = lws, exdent = lws)
+    replacement_text <- my_wrap(tmp$contents[start_pos[1]], width = 70,
+                                indent = lws, exdent = lws)
     current_length <- nchar(tmp$contents[start_pos[1]])
     replacement_range <- rstudioapi::document_range(start = c(start_pos[1], 1),
                                                     end = c(start_pos[1], current_length + 1))
@@ -27,8 +38,8 @@ reflowLines <- function () {
 
       replacement_text <- paste0(tmp$contents[start_pos[1]:(end_pos[1] - 1)],
                                  collapse = ' ')
-      replacement_text <- stringr::str_wrap(replacement_text, width = 70,
-                                            indent = lws, exdent = lws)
+      replacement_text <- my_wrap(replacement_text, width = 70,
+                                  indent = lws, exdent = lws)
       last_length <- nchar(tmp$contents[end_pos[1] - 1])
       replacement_range <- rstudioapi::document_range(start = c(start_pos[1], 1),
                                                       end = c(end_pos[1] - 1, last_length + 1))
@@ -36,8 +47,8 @@ reflowLines <- function () {
 
       replacement_text <- paste0(tmp$contents[start_pos[1]:end_pos[1]],
                                  collapse = ' ')
-      replacement_text <- stringr::str_wrap(replacement_text, width = 70,
-                                            indent = lws, exdent = lws)
+      replacement_text <- my_wrap(replacement_text, width = 70,
+                                  indent = lws, exdent = lws)
       last_length <- nchar(tmp$contents[end_pos[1]])
       replacement_range <- rstudioapi::document_range(start = c(start_pos[1], 1),
                                                       end = c(end_pos[1], last_length + 1))
